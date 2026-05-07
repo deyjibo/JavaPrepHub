95a986315ceaf8762e536299357b59c3e91ce569
deyjibo <deyjibo56@gmail.com>
2026-05-08 00:51:42 +0530

    Day 1: Java basics start

--- Day1/Simple Question/Operation.java
+++ Day1/Simple Question/Operation.java
@@ -7,6 +7,15 @@ public class Operation {
         System.out.println(a - b);
         System.out.println(a * b);
         System.out.println(a / b);
+        Math.sqrt(a); // square root
+        Math.pow(a, b); // power
+        Math.abs(a); // absolute value
+        Math.max(a, b); // maximum
+        Math.min(a, b); // minimum
+        Math.random(); // random number
+        Math.round(a); // round
+        Math.ceil(a); // upper value
+        Math.floor(a); // lower value
 
     }
 }
