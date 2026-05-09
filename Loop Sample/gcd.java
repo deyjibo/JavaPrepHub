db6e52ee235589695f217ad8a45720914745387e
deyjibo <deyjibo56@gmail.com>
2026-05-09 20:35:48 +0530

    Day 2: Java loops

--- Loop Sample/gcd.java
+++ Loop Sample/gcd.java
@@ -1,7 +1,15 @@
 public class gcd {
     public static void main(String[] args) {
-        int a=12,b=18;
-        int gcd=1;
-        
+        int a = 24, b = 36;
+        int on1=a,on2=b;
+        while (b % a != 0) {
+            int rem = b % a;
+            b = a;
+            a = rem;
+        }
+        int gcd=a;
+        int lcm=(on1*on2)/gcd;
+        System.out.println("GCD is:" + gcd);
+        System.out.println("LCM is:" + lcm);
     }
 }
