e5b12d9bf5eec6d214f6ca12e3bc91db6335004c
deyjibo <deyjibo56@gmail.com>
2026-05-10 03:13:28 +0530

    Day 3:Java Array

---
+++ array Questions/equal_arr.java
@@ -0,0 +1,17 @@
+public class equal_arr {
+    public static void main(String[] args) {
+        int arr1[] = { 1, 2, 3 };
+        int arr2[] = { 1, 2, 3 };
+        boolean equal = true;
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                equal = false;
+                break;
+            }
+        }
+        if (equal)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
--- array Questions/remove_duplicates.java
+++ array Questions/remove_duplicates.java
@@ -1,4 +1,4 @@
-public class duplicates {
+public class remove_duplicates {
     public static void main(String[] args) {
         int arr[]={1,1,2,2,3,3,4,4,4,5,5};
         for(int i=0;i<arr.length-1;i++){
